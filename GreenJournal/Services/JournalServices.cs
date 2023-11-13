ef9a82ef7a72188f2814ffe4034cb61368900bf5
TheZenithTree <zenithrowankile@gmail.com>
2023-11-13 02:27:05 -0600

    Updated NuGet packages Added SQLite-net-pcl nuget package Added JournalServices.cs

---
+++ GreenJournal/Services/JournalServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenJournal.Services
+{
+    class JournalServices
+    {
+        public static async Task Init()
+        {
+
+        }
+
+        public static async Task AddEntry()
+        {
+
+        }
+
+        public static async Task DeleteEntry()
+        {
+
+        }
+
+        public static async Task GetEntry()
+        {
+
+        }
+    }
+}
