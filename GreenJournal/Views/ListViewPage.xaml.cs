f68bc5a527de4a3c7a37b93e7283804e0c156dfe
Saori Kojima <skojima@atu.edu>
2023-11-27 15:22:18 -0600

    Created a simple list view.

--- GreenJournal/Views/ListViewPage.xaml.cs
+++ GreenJournal/Views/ListViewPage.xaml.cs
@@ -1,20 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace GreenJournal.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListViewPage : ContentPage
     {
         public ListViewPage()
         {
             InitializeComponent();
+
+            // ********This is a test data for debugging list view********
+            var journalEntries = new ObservableCollection<JournalEntry>
+            {
+                new JournalEntry { Date = DateTime.Now, Text = "I love driving." },
+                new JournalEntry { Date = DateTime.Now.AddDays(-1), Text = "I went to Dallas." },
+                new JournalEntry { Date = DateTime.Now.AddDays(-5), Text = "It was a nice day!" },
+                new JournalEntry { Date = DateTime.Now.AddDays(-6), Text = "I went to school!" },
+            };
+
+            // Bind the data to ListView
+            journalListView.ItemsSource = journalEntries;
         }
+    }
+
+    // Define JournalEntry class
+    public class JournalEntry
+    {
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
     }
 }
