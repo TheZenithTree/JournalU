30a8a326bcaef79500ae7b761079359eb008179a
TheZenithTree <zenithrowankile@gmail.com>
2023-11-27 02:06:26 -0600

    Setting event handlers and variables
    Current Issue: x:Name not creating field for .cs file

--- GreenJournal/Views/SettingsPage.xaml.cs
+++ GreenJournal/Views/SettingsPage.xaml.cs
@@ -12,15 +12,55 @@ namespace GreenJournal.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SettingsPage : ContentPage
     {
-        public SettingsPage()
+       public SettingsPage()
         {
             InitializeComponent();
             //BindingContext = new SettingsPageVM();
 
-            
+        }
+
+        bool showPasswordValue = false;
+        int currentPassword = 0000;
+        bool enableReminder = false;
+
+        public int CurrentPassword
+        {
+            get => currentPassword;
+
+            set
+            {
+                if (value == currentPassword)
+                    return;
+                currentPassword = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void showPasswordCell_OnChanged(object sender, ToggledEventArgs e)
+        {
+            if (showPasswordCell.IsEnabled == true)
+            {
+                showPasswordValue = true;
+            }
+            else
+                showPasswordValue = false;
 
         }
 
-        
+
+        private void enableReminderCell_OnChanged(object sender, ToggledEventArgs e)
+        {
+            if (e.Value == true) 
+            {
+                enableReminder = true;
+            }
+            else
+                enableReminder = false;
+        }
+
+        private void setPasswordCell_Completed(object sender, EventArgs e)
+        {
+            currentPassword = 
+        }
     }
 }
