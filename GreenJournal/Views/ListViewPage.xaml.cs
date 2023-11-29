b7770f380eb1c44ed9be7c6c009362d7cb0be3c7
Saori Kojima <skojima@atu.edu>
2023-11-29 16:40:04 -0600

    Connected to hte actual database in the list view

--- GreenJournal/Views/ListViewPage.xaml.cs
+++ GreenJournal/Views/ListViewPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using Xamarin.Forms;
+using GreenJournal.Models;
+using GreenJournal.ViewModels;
 
 namespace GreenJournal.Views
 {
@@ -10,24 +12,19 @@ namespace GreenJournal.Views
         {
             InitializeComponent();
 
-            // ********This is a test data for debugging list view********
-            var journalEntries = new ObservableCollection<JournalEntry>
-            {
-                new JournalEntry { Date = DateTime.Now, Text = "I love driving." },
-                new JournalEntry { Date = DateTime.Now.AddDays(-1), Text = "I went to Dallas." },
-                new JournalEntry { Date = DateTime.Now.AddDays(-5), Text = "It was a nice day!" },
-                new JournalEntry { Date = DateTime.Now.AddDays(-6), Text = "I went to school!" },
-            };
+            BindingContext = new JournalViewModel();
 
-            // Bind the data to ListView
-            journalListView.ItemsSource = journalEntries;
+            //// ********This is a test data for debugging list view********
+            //var journalEntries = new ObservableCollection<Models.Entry>
+            //{
+            //    new Models.Entry { Time = DateTime.Now, Content = "I love driving." },
+            //    new Models.Entry { Time = DateTime.Now.AddDays(-1), Content = "I went to Dallas." },
+            //    new Models.Entry { Time = DateTime.Now.AddDays(-5), Content = "It was a nice day!" },
+            //    new Models.Entry { Time = DateTime.Now.AddDays(-6), Content = "I went to school!" },
+            //};
+
+            //// Bind the data to ListView
+            //journalListView.ItemsSource = journalEntries;
         }
-    }
-
-    // Define JournalEntry class
-    public class JournalEntry
-    {
-        public DateTime Date { get; set; }
-        public string Text { get; set; }
     }
 }
